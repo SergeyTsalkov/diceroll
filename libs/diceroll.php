c7f5046aaab9f00759d9dec7f7676a4cebe7e812
Sergey Tsalkov <stsalkov@gmail.com>
2022-12-08 23:16:38 +0000

    diceroll() now properly uses random.org through random()

--- libs/diceroll.php
+++ libs/diceroll.php
@@ -2,11 +2,27 @@
 
 function diceroll($string) {
   $parts = preg_split('/([+-])/', $string, -1, PREG_SPLIT_DELIM_CAPTURE);
-  
+
+  // figure out which random numbers we need
+  $need_random = [];
+  foreach ($parts as $part) {
+    if (!preg_match('/(\d+)?d(\d+)/', $part, $match)) continue;
+
+    $max = intval($match[2]);
+    $count = intval($match[1]);
+    if ($count < 1) $count = 1;
+
+    $need_random[$max] += $count;
+  }
+
+  // get random numbers
+  $random = random($need_random);
+  var_dump($random);
+
+  // use random numbers to generate intermediate string and result
   $intermediate = [];
   $result = 0;
   $add = true;
-
   foreach ($parts as &$part) {
     $part = trim($part);
 
@@ -17,7 +33,8 @@ function diceroll($string) {
 
       $rands = [];
       for ($i = 0; $i < $count; $i++) {
-        $rands[] = random_int(1, $max);
+        if (count($random[$max]) < 1) throw new Exception("Unable to parse: not enough random numbers");
+        $rands[] = array_shift($random[$max]);
       }
 
       if (!is_bool($add)) throw new Exception();
@@ -64,7 +81,6 @@ function random(array $requests): array {
   $random = [];
   foreach ($requests as $max => $count) {
     if ($waitfor) {
-      echo "waiting for $waitfor ms..\n";
       usleep($waitfor * 1000);
     }
 
